fc497ebdf0eced31e638780a3bcd3542528954ce
Sergey <Semaserg@gmail.com>
2016-07-28 10:09:45 -0700

    added O(log n ) solution

--- array/FindMinimumInRotatedSortedArray_153/Solution.java
+++ array/FindMinimumInRotatedSortedArray_153/Solution.java
@@ -29,11 +29,14 @@ public class Solution {
     public int findMin(int[] nums) {
         if (nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
-        for(int i=0; i<nums.length-1; i++) {
-            if (nums[i]>nums[i+1]) {
-                return nums[i+1];
-            }
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right) {
+            if(nums[left] < nums[right]) return nums[left];
+            int mid = (left + right)/2;
+            if (nums[mid] > nums[right]) left = mid+1;
+            else right = mid;
         }
-        return nums[0];
+        return nums[left];
     }
 }
