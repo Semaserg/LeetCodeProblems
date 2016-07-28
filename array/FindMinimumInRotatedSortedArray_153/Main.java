f3a183adf7896d0f04b92838f1054bf9dfc714a0
Sergey <Semaserg@gmail.com>
2016-07-27 22:33:14 -0700

    added O(n) solution for "Find minimum in rotated sorted array" problem

---
+++ array/FindMinimumInRotatedSortedArray_153/Main.java
@@ -0,0 +1,12 @@
+package LeetCode.array.FindMinimumInRotatedSortedArray_153;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int min = s.findMin(nums);
+        System.out.print(min);
+    }
+}
+
+
---
+++ array/FindMinimumInRotatedSortedArray_153/Solution.java
@@ -0,0 +1,39 @@
+package LeetCode.array.FindMinimumInRotatedSortedArray_153;
+
+/**
+ 153. Find Minimum in Rotated Sorted Array
+ https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+ Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+ (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+ Find the minimum element.
+
+ You may assume no duplicate exists in the array.
+ */
+public class Solution {
+    // O(n) time complexity
+    public int findMin1(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        for(int i=0; i<nums.length-1; i++) {
+            if (nums[i]>nums[i+1]) {
+                return nums[i+1];
+            }
+        }
+        return nums[0];
+    }
+
+    // O(log n) time complexity - binary search
+    public int findMin(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        for(int i=0; i<nums.length-1; i++) {
+            if (nums[i]>nums[i+1]) {
+                return nums[i+1];
+            }
+        }
+        return nums[0];
+    }
+}
