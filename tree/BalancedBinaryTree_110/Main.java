2c878399e635fc147718ae6649700571af1efe37
Sergey <Semaserg@gmail.com>
2016-08-18 18:44:48 -0700

    added 110 problem-solution

---
+++ tree/BalancedBinaryTree_110/Main.java
@@ -0,0 +1,16 @@
+package LeetCode.tree.BalancedBinaryTree_110;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        root.left.right.left = new TreeNode(10);
+
+        boolean result = s.isBalanced(root);
+        System.out.print(result);
+    }
+}
---
+++ tree/BalancedBinaryTree_110/Solution.java
@@ -0,0 +1,40 @@
+package LeetCode.tree.BalancedBinaryTree_110;
+
+/**
+ 110. Balanced Binary Tree
+ https://leetcode.com/problems/balanced-binary-tree/
+
+ Given a binary tree, determine if it is height-balanced.
+
+ For this problem, a height-balanced binary tree is defined
+ as a binary tree in which the depth of the two subtrees of
+ every node never differ by more than 1.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// Good description
+// https://discuss.leetcode.com/topic/7798/the-bottom-up-o-n-solution-would-be-better
+public class Solution {
+    private boolean isBal = true;
+
+    public boolean isBalanced(TreeNode root) {
+        checkDepth(root);
+        return isBal;
+    }
+
+    private int checkDepth(TreeNode root) {
+        if (root == null) return 0;
+        int lh = checkDepth(root.left);
+        int lr = checkDepth(root.right);
+        if (Math.abs(lh - lr)>1) isBal = false;
+        return Math.max(lh, lr) + 1;
+    }
+}
