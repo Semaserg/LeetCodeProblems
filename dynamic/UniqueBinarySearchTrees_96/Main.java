7fd8047be61eb7e2f5c1482ffd28c8c9aa66dc64
Sergey <Semaserg@gmail.com>
2016-08-22 16:10:40 -0700

    added 96 problem-solution

---
+++ dynamic/UniqueBinarySearchTrees_96/Main.java
@@ -0,0 +1,11 @@
+package LeetCode.dynamic.UniqueBinarySearchTrees_96;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution n = new Solution();
+        int result = n.numTrees(4);
+        System.out.println(result);
+    }
+}
+
+
---
+++ dynamic/UniqueBinarySearchTrees_96/Solution.java
@@ -0,0 +1,52 @@
+package LeetCode.dynamic.UniqueBinarySearchTrees_96;
+
+/*
+96. Unique Binary Search Trees
+https://leetcode.com/problems/unique-binary-search-trees/
+
+Given n, how many structurally unique BST's
+(binary search trees) that store values 1...n?
+
+For example,
+Given n = 3, there are a total of 5 unique BST's.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+*/
+
+public class Solution {
+    // Good explanation
+    // https://discuss.leetcode.com/topic/37310/fantastic-clean-java-dp-solution-with-detail-explaination
+    // https://discuss.leetcode.com/topic/8398/dp-solution-in-6-lines-with-explanation-f-i-n-g-i-1-g-n-i
+    // Time complexity O(n^2), space complexity O(n).
+    public int numTrees(int n) {
+        // dp - num of unique trees for 0, 1 ... n items in the tree.
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i=2; i<=n; i++) {
+            for (int j=1; j<=i; j++){
+                dp[i] += dp[j-1] * dp[i-j];
+            }
+        }
+        return dp[n];
+    }
+//
+//    public int numTrees(int n) {
+//        return numTreesRecursive(1, n);
+//    }
+//
+//    private int numTreesRecursive(int min, int max) {
+//        if (min == max) return 1;
+//        int sum = 0;
+//        for (int i=min; i<=max; i++) {
+//            int left = (i > min) ? numTreesRecursive(min, i-1) : 0;
+//            int right = (i < max) ? numTreesRecursive(i+1, max) : 0;
+//            sum += left * right;
+//        }
+//        return sum;
+//    }
+}
