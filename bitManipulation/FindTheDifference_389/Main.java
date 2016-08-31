616adb8819ab4ec30169f3be4472dec19f7528c6
Sergey <Semaserg@gmail.com>
2016-08-30 19:26:37 -0700

    added 389 problem-solution

---
+++ bitManipulation/FindTheDifference_389/Main.java
@@ -0,0 +1,9 @@
+package LeetCode.bitManipulation.FindTheDifference_389;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        char result = s.findTheDifference("abcd", "abcde") ;
+        System.out.println(result);
+    }
+}
