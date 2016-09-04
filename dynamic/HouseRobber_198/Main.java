01587f83d58ee8fcfe9a9ca4c80182eea951f6b4
Sergey <Semaserg@gmail.com>
2016-09-03 21:07:30 -0700

    added 198 problem-solution

--- dynamic/HouseRobber_198/Main.java
+++ dynamic/HouseRobber_198/Main.java
@@ -3,7 +3,7 @@ package LeetCode.dynamic.HouseRobber_198;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {5,10,2,12};
+        int[] nums = {10,1,2,12};
         int result = s.rob(nums);
         System.out.print(result);
     }
--- dynamic/HouseRobber_198/Solution.java
+++ dynamic/HouseRobber_198/Solution.java
@@ -1,6 +1,5 @@
 package LeetCode.dynamic.HouseRobber_198;
 
-import java.util.HashMap;
 
 /*
 198. House Robber
@@ -20,39 +19,35 @@ public class Solution {
     // Time limit exceeded
     public int rob1(int[] nums) {
         if (nums.length == 0) return 0;
-        return robRecursive1(0, nums);
-    }
-
-    private int robRecursive1(int index, int[] nums) {
-        int lastIndex = nums.length-1;
-        if (index>lastIndex) return 0;
-        int a = nums[index] + robRecursive1(index+2, nums);
-        int b = 0;
-        int nextIndex = index + 1;
-        if (nextIndex <= lastIndex) {
-            b = nums[nextIndex] + robRecursive1(nextIndex+2, nums);
-        }
-        return Math.max(a,b);
-    }
-
-    // Time limit exceeded
-    // Looks like time complexity O(2^n), n==nums.length;
-    public int rob(int[] nums) {
-        if (nums.length == 0) return 0;
         return robRecursive(0, nums);
     }
 
     /*
-        M(k) = money at the kth house
-        P(0) = 0
-        P(1) = M(1)
-        P(k) = max(P(k−2) + M(k), P(k−1))  */
+    M(k) = money at the kth house
+    P(0) = 0
+    P(1) = M(1)
+    P(k) = max(P(k−2) + M(k), P(k−1))  */
     private int robRecursive(int index, int[] nums) {
-        int lastIndex = nums.length-1;
-        if (index>lastIndex) return 0;
-        if (index==lastIndex) return nums[index];
-        return Math.max(nums[index]+robRecursive(index+2,nums), robRecursive(index+1,nums));
+        if (index>=nums.length) return 0;
+        return Math.max(nums[index] + robRecursive(index+2, nums), robRecursive(index+1, nums));
     }
+
+    // https://discuss.leetcode.com/topic/11110/c-1ms-o-1-space-very-simple-solution
+    // https://discuss.leetcode.com/topic/12024/java-dp-solution-o-n-runtime-and-o-1-space-with-inline-comment
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        int a = 0;
+        int b = 0;
+        for (int i=0; i<nums.length; i++) {
+            if (i%2 ==0) {
+                a = Math.max(a + nums[i], b);
+            } else {
+                b = Math.max(b + nums[i], a);
+            }
+        }
+        return Math.max(a, b);
+    }
+
 
     // Good solutions, not recursive.
     // https://discuss.leetcode.com/topic/12024/java-dp-solution-o-n-runtime-and-o-1-space-with-inline-comment
