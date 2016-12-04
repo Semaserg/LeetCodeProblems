9e2e415c6e4297b673f2587aa139b29bb4eeced7
Sergey <Semaserg@gmail.com>
2016-12-03 16:06:27 -0800

    added 277 problem-solution

---
+++ array/FindTheCelebrity_277/Main.java
@@ -0,0 +1,11 @@
+package LeetCode.array.FindTheCelebrity_277;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int res = s.findCelebrity(5);
+        System.out.print(res);
+    }
+}
+
+
---
+++ array/FindTheCelebrity_277/Relation.java
@@ -0,0 +1,10 @@
+package LeetCode.array.FindTheCelebrity_277;
+
+/**
+ * Created by Sergii on 03.12.2016.
+ */
+public class Relation {
+    boolean knows(int a, int b) {
+        return false;
+    }
+}
