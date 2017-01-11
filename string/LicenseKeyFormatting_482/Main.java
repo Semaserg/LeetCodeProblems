2f9bb7eaf533aeeb6f5d4cf08715357dd5396c5b
Sergey <Semaserg@gmail.com>
2017-01-10 21:52:25 -0800

    added 482 problem-solution

---
+++ string/LicenseKeyFormatting_482/Main.java
@@ -0,0 +1,9 @@
+package LeetCode.string.LicenseKeyFormatting_482;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String result = s.licenseKeyFormatting("2-4A0r7-4k", 3);
+        System.out.print(result);
+    }
+}
