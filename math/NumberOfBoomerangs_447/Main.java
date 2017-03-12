38e46cc206477acf4c749f11ce7bcb0e7e2784e1
Sergey <Semaserg@gmail.com>
2017-03-11 16:26:06 -0800

    added 447 problem-solution

---
+++ math/NumberOfBoomerangs_447/Main.java
@@ -0,0 +1,9 @@
+package LeetCode.math.NumberOfBoomerangs_447;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int result = s.numberOfBoomerangs(new int[][]{{0,1}, {1,1}, {1,0}}) ;
+        System.out.println(result);
+    }
+}
---
+++ math/NumberOfBoomerangs_447/Solution.java
@@ -0,0 +1,55 @@
+package LeetCode.math.NumberOfBoomerangs_447;
+
+import java.util.HashMap;
+
+/**
+447. Number of Boomerangs
+https://leetcode.com/problems/number-of-boomerangs/?tab=Description
+
+Given n points in the plane that are all pairwise distinct, a "boomerang"
+is a tuple of points (i, j, k) such that the distance between i and j equals
+the distance between i and k (the order of the tuple matters).
+
+Find the number of boomerangs. You may assume that n will be at most 500 and
+coordinates of points are all in the range [-10000, 10000] (inclusive).
+
+Example:
+Input:
+[[0,0],[1,0],[2,0]]
+
+Output:
+2
+
+Explanation:
+The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
+Show Company Tags
+Hide Tags
+*/
+// https://discuss.leetcode.com/topic/66587/clean-java-solution-o-n-2-166ms
+//
+public class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        if (points == null) throw new IllegalArgumentException("points should not be null");
+        if (points.length < 3) return 0;
+        int counter = 0;
+
+        for (int[] fp : points) {
+            HashMap<Integer, Integer> dist = new HashMap<>(); // <distance, counter>
+            for (int[] sp : points) {
+                if (fp == sp) continue;
+                int squareDistance = (fp[0]-sp[0])*(fp[0]-sp[0]) + (fp[1]-sp[1])*(fp[1]-sp[1]);
+                if (!dist.containsKey(squareDistance)) {
+                    dist.put(squareDistance, 1);
+                } else {
+                    int nextDistCounter = dist.get(squareDistance) + 1;
+                    dist.put(squareDistance, nextDistCounter);
+
+                    // can be changed with iteration hashmap after the loop and adding cnt*(cnt-1) to global counter.
+                    // counter += cnt*(cnt-1)
+                    counter += 2 * (nextDistCounter - 1);
+                }
+            }
+        }
+        return counter;
+    }
+}
