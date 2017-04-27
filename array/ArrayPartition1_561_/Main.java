d33bbd1d1de16c7d7867830a06cded743116ce0b
Sergey <Semaserg@gmail.com>
2017-04-26 21:33:58 -0700

    added 561 problem-solution

---
+++ array/ArrayPartition1_561_/Main.java
@@ -0,0 +1,12 @@
+package LeetCode.array.ArrayPartition1_561_;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {7,3,1,0,0,6};
+        int result = s.arrayPairSum(arr);
+        System.out.print(result);
+    }
+}
+
+
