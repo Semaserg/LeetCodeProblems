80d0423b77bd8881822a3fe9a2306e71e791433c
Sergey <Semaserg@gmail.com>
2017-04-26 20:54:55 -0700

    added 419 problem-solution

---
+++ array/BattleshipsInABoard_419_/Main.java
@@ -0,0 +1,12 @@
+package LeetCode.array.BattleshipsInABoard_419_;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        char[][] board = {{'X', 'X', 'X', '.', 'X'}};
+        int result = s.countBattleships(board);
+        System.out.print(result);
+    }
+}
+
+
