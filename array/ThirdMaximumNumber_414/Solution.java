74f928e368481fcb1264407fa7a5d15ffc7535e9
Sergey <Semaserg@gmail.com>
2017-06-04 15:48:24 -0700

    added new solution to 414 problem

--- array/ThirdMaximumNumber_414/Solution.java
+++ array/ThirdMaximumNumber_414/Solution.java
@@ -31,7 +31,7 @@ import java.util.List;
  Both numbers with value 2 are both considered as second maximum.
  */
 public class Solution {
-    public int thirdMax(int[] nums) {
+    public int thirdMax1(int[] nums) {
         Integer[] top3 = new Integer[3]; // initialized array contains nulls
         for (int i : nums) trySet(top3, i);
         return top3[2] != null ? top3[2] : top3[0];
@@ -53,5 +53,23 @@ public class Solution {
                 current = temp;
             }
         }
+    }
+
+    Integer getMaxLowerThan(int[] nums, Integer top) {
+        Integer result = null;
+        for(int i : nums) {
+            if ((top == null || i < top) && (result == null || result < i)) result = i;
+        }
+        return result;
+    }
+
+    public int thirdMax(int[] nums) {
+        Integer first = getMaxLowerThan(nums, null);
+        if (first == null) return 0;
+        Integer second = getMaxLowerThan(nums, first);
+        if (second == null) return first;
+        Integer third = getMaxLowerThan(nums, second);
+        if (third == null) return first;
+        return third;
     }
 }
