096aaa24da46eb1f4b7c86de5495493b69ca860b
Sergey <Semaserg@gmail.com>
2017-06-17 10:46:15 -0700

    added 259 problem-solution

---
+++ array/ThreeSumSmaller_259/Main.java
@@ -0,0 +1,15 @@
+package LeetCode.array.ThreeSumSmaller_259;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int res = s.threeSumSmaller(new int[] {-2, 0, 3, 1}, 2);
+        System.out.print(res);
+    }
+}
+
+
