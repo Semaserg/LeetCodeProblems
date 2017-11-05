7573ee4a8c268f399c90fe819fa887fb5f00877e
Sergey <Semaserg@gmail.com>
2017-11-04 18:24:20 -0700

    updated 69

--- binarySearch/Sqrt_69/Solution.java
+++ binarySearch/Sqrt_69/Solution.java
@@ -27,12 +27,16 @@ public class Solution {
     // https://discuss.leetcode.com/topic/8680/a-binary-search-solution
     public int mySqrt(int x) {
         if (x == 0) return 0;
-        int lo = 0, hi = x;
-        while(lo < hi) {
+        int lo = 1, hi = x;
+        while(lo <= hi) { // important to use <= , for instance x == 1.
             int mid = lo + (hi - lo)/2;
-            if (x/mid > mid) lo = mid + 1;
-            else hi = mid;
+            if (mid > x/mid) {
+                hi = mid - 1;
+            } else {
+                if ((mid+1) > x/(mid+1)) return mid;
+                lo = mid + 1;
+            }
         }
-        return hi-1;
+        throw new IllegalStateException("ex");
     }
 }
