986aef79e2cbdacd3ef5b3c3b1decd4a361da79c
Sergey <Semaserg@gmail.com>
2017-11-09 20:53:11 -0800

    added quick select

--- design/QuickSort/Main.java
+++ design/QuickSort/Main.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //Integer[] arr = new Integer[]{0,1,9,2,8,3,7,4,4,4,4,4,5,6};
-        Integer[] arr = new Integer[]{};
+        Integer[] arr = new Integer[]{0,1,9,2,8,3,7,4,4,4,4,4,5,6};
+        //Integer[] arr = new Integer[]{};
+        System.out.println(QuickSort.select(arr, 6));
         QuickSort.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
--- design/QuickSort/QuickSort.java
+++ design/QuickSort/QuickSort.java
@@ -6,6 +6,23 @@ public class QuickSort{
         sortArray(array, 0, array.length-1);
     }
 
+    // https://en.wikipedia.org/wiki/Quickselect
+    // k-th smallest element
+    // O(n) time complexity
+    public static Comparable select(Comparable[] array, int k) {
+        if (array == null) throw new IllegalStateException("ex");
+        if (k >= array.length) throw new IllegalStateException("ex");
+        return kthSmallest(array, k, 0, array.length-1);
+    }
+
+    private static Comparable kthSmallest(Comparable[] array, int k, int lo, int hi) {
+        if (lo == hi) return lo;
+        int pivotInd = partition(array, lo, hi);
+        if (pivotInd == k) return array[pivotInd];
+        if (pivotInd > k) return kthSmallest(array, k, lo, pivotInd-1);
+        else return kthSmallest(array, k,pivotInd+1, hi);
+    }
+
     private static void sortArray(Comparable[] array, int lo, int hi) {
         if (lo < hi) {
             int pivot = partition(array, lo, hi);
